b0059a03178dd1d7c54c34c1c6278cebd08f67eb
Alexyr28 <alexyr24@gmail.com>
2025-08-01 13:04:51 -0500

    feat: Clase TiposDatosyCondicion

---
+++ Full-Stack/CarreraDesarrolladorWeb/JavaScript/js/05-Tipos_Datos.js
@@ -0,0 +1,37 @@
+// TIPOS DATOS
+
+var numero_entero = 44;
+var numero_decimal = 44.44;
+var texto = "Hola mundo";
+var texto2 = "'Hola mundo'";
+var texto3 = '"Hola mundo"';
+
+//BOOLEANO
+var verdadero = true;
+var falso = false;
+var nulo = null;
+
+var indefinido = undefined;
+var indefinido;
+
+console.log(texto, texto2, indefinido);
+
+//VER EL TIPO DE DATO
+console.log(typeof(numero_decimal));
+var numero_false = "22.3";
+console.log(numero_false+5);
+
+//CONVERITR A TEXTO A NUMERO
+console.log(Number(numero_false)+5);
+console.log(parseInt(numero_false)+6);
+console.log(parseFloat(numero_false)+7.4);
+
+var numero_false2 = 22;
+
+//CONVERTIR NUMERO A TEXTO
+console.log(String(numero_false2)+2);
+
+
+
+
+
---
+++ Full-Stack/CarreraDesarrolladorWeb/JavaScript/js/06-condicionales.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//CONDICIONAL IF
+var edad = 11;
+var nombre = "Juan";
+var año = 2023;
+
+if (edad == 11)
+{
+    console.log("La edad es igual a 11");
+    
+}
