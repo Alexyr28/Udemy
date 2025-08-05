ace5401ad9e7fff7b244d46f3af4953dd7886522
Alexyr28 <alexyr24@gmail.com>
2025-08-05 12:38:25 -0500

    feat: Avance Ejercicio

--- Full-Stack/CarreraDesarrolladorWeb/JavaScript/js/12-Ejercicio1.js
+++ Full-Stack/CarreraDesarrolladorWeb/JavaScript/js/12-Ejercicio1.js
@@ -1,13 +1,34 @@
 'use strict'
 
-/* Programa que pida dos numeros y que diga cual es mayor y cual es menor y si son iguales; si los numeros no son numero o son menores o son iguales a 0, volver a pedir los numeros */
+/* Programa que pida dos numeros y que diga cual es mayor y cual es menor y si son iguales; si los numeros no son numero o son negativos o son iguales a 0, volver a pedir los numeros */
 
 
 //Ingreso Primer Número
-do{
-    var numero1 = parseInt(prompt("Ingrese un Número"));
-    var bandera1 = true;
-}while(bandera1==false)
+var bandera1 = true;
+while(bandera1){
+    var numero1 = parseInt(prompt("Ingrese un Número", 0));
+    if(numero1<=0){
+        bandera1 = true;
+    }else if(isNaN(numero1)){
+        bandera1 = true;
+    }else{
+        bandera1 = false;
+    }
+}
 
+//Ingreso Segundo Número
+var bandera2 = true;
 
-var numero2 = parseInt(prompt("Ingrese otro Número"));
+while(bandera2){
+    var numero2 = parseInt(prompt("Ingrese otro Número", 0));
+    if(numero2<=0){
+        bandera2 = true;
+    }else if(isNaN(numero2)){
+        bandera2 = true;
+    }else{
+        bandera2 = false;
+    }
+}
+
+//Validación
+
