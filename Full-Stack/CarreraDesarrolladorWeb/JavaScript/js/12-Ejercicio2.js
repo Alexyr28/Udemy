b48965c6d552c5a19a9dd6bb1f826c20178eb745
Alexyr28 <alexyr24@gmail.com>
2025-08-07 10:33:41 -0500

    feat: Ejecicio2

--- Full-Stack/CarreraDesarrolladorWeb/JavaScript/js/12-Ejercicio2.js
+++ Full-Stack/CarreraDesarrolladorWeb/JavaScript/js/12-Ejercicio2.js
@@ -3,8 +3,8 @@
 /* Utilizar un bucle para calcular la suma y la media de los numeros introducidos hasta introducir un numero negativo y ahi mostrar el resultado */
 
 var suma = 0;
-var numero = -6;
-var bandera = true;
+var numero = 0;
+var bandera = false;
 
 while(numero >= 0){
     numero = parseInt(prompt("Introduce un numero: "));
