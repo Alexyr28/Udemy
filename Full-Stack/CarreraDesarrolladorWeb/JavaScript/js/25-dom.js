4bcc5027d1e1202da46c4e01bfd6d0d5ebb2d1b6
Alexyr28 <alexyr24@gmail.com>
2025-10-06 18:55:36 -0500

    feat: Clase Dom 1 y 2

--- Full-Stack/CarreraDesarrolladorWeb/JavaScript/js/25-dom.js
+++ Full-Stack/CarreraDesarrolladorWeb/JavaScript/js/25-dom.js
@@ -8,3 +8,47 @@ se conoce como DOM (Document Object Model)
 */
 var caja = document.getElementById('micaja');
 console.log(caja);
+caja.style.background = 'red';
+
+//Otra manera de elegir ID
+var caja1 = document.querySelector("#micaja");
+
+function cambiarColor(color){
+    caja1.style.background = color;
+}
+
+cambiarColor('blue');
+//QuerySelector selecciona el primer elemento que coincida con el selector CSS especificado
+var etiqueta = document.querySelector("h1");
+etiqueta.style.background = 'green';
+
+//InderHTML Recoge el contenido actual de un contenedor o inserta nuevo contenido dentro de un contenedor
+var caja3 = document.getElementById("micaja").innerHTML;
+console.log(caja3);
+
+var caja4 = document.getElementById("micaja");
+caja4.innerHTML = "El contenido de la caja fue cambiado";
+console.log(caja4);
+
+caja4.style.padding = "20px";
+caja4.style.color = "white";
+caja4.style.background = "green";
+caja4.className = "mi-clase-caja";
+
+console.log(caja4);
+
+//CONSEGUIR ELEMENTOS POR SU ETIQUETA
+//getElementByIdTagName ingresa convirtiendo un array a todos los elementos que coincidan con el nombre de etiqueta
+var todoslosdiv = document.getElementsByTagName("div");
+console.log(todoslosdiv);
+
+var contenido = todoslosdiv[2].textContent;
+console.log(contenido);
+
+var contenido = todoslosdiv[2]
+contenido.innerHTML = "Hola soy Alexyr"
+
+
+
+
+
